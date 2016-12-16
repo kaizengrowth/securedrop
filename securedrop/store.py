08c3034b6cde5b659eb8adba6ae762e3c7e0d8ff
redshiftzero <jen@redshiftzero.com>
2016-12-16 14:27:39 -0800

    Add comments and minor variable name changes

--- securedrop/store.py
+++ securedrop/store.py
@@ -68,24 +68,26 @@ def path(*s):
     return absolute
 
 
-def get_bulk_archive(items_selected, zip_directory=''):
+def get_bulk_archive(selected_submissions, zip_directory=''):
+    """Generate a zip file from the selected submissions"""
     zip_file = tempfile.NamedTemporaryFile(prefix='tmp_securedrop_bulk_dl_',
                                            dir=config.TEMP_DIR,
                                            delete=False)
-    sources = set([i.source.journalist_designation for i in items_selected])
+    sources = set([i.source.journalist_designation for i in selected_submissions])
     # The below nested for-loops are there to create a more usable
     # folder structure per #383
     with zipfile.ZipFile(zip_file, 'w') as zip:
         for source in sources:
-            submissions = [s for s in items_selected if s.source.journalist_designation == source]
-            for index, submission in enumerate(submissions, start=1):
+            submissions = [s for s in selected_submissions if s.source.journalist_designation == source]
+            for submission in submissions:
                 filename = path(submission.source.filesystem_id,
                                 submission.filename)
                 verify(filename)
                 zip.write(filename, arcname=os.path.join(
                     zip_directory,
                     source,
-                    "%s_%s" % (index, submission.source.last_updated.date()),
+                    "%s_%s" % (submission.filename.split('_')[0],
+                               submission.source.last_updated.date()),
                     os.path.basename(filename)
                 ))
     return zip_file
